2d431852848378d4f6724c05b63dc03d2ea2d8f9
Frank1963-mpoyi <frankmpoyi63@gmail.com>
2021-01-04 10:36:23 +0200

    new

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ DEBUG = config('DEBUG', cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.heroku.com', 'mpoyifrank.herokuapp.com']
 
 
 # Application definition
